1fd73503766b00a78ad7dd25de49fada31de457c
Unknown <goldfedertomer@gmail.com>
2018-12-08 15:45:04 +0200

    added stars to movie infos

--- pictures.js
+++ pictures.js
@@ -11,56 +11,56 @@ var pictures_info = {
 
 var movies_infos = {
   "Final Score (2018)" : {
-    "ratings": "5/10",
+    "ratings": 2,
     "released": "2018",
     "genre": "Action",
     "plot": "After deadly terrorists abduct his niece at a soccer match, an ex-soldier with lethal fighting skills wages a one-man war to save her and prevent mass destruction.",
     "actors": "Aaron McCusker, Dave Bautista, Pierce Brosnan."
   },
   "Mission Impossible (2018)" : {
-    "ratings": "8/10",
+    "ratings": 4,
     "released": "2018",
     "genre": "Action, Adventure, Thriller",
     "plot": "Mission: Impossible - Fallout is a movie starring Tom Cruise, Henry Cavill, and Ving Rhames. Ethan Hunt and his IMF team, along with some familiar allies, race against time after a mission gone wrong.",
     "actors": "Tom Cruise, Henry Cavill, Ving Rhames, Simon Pegg."
   },
   "Ocean's Eight (2018)" : {
-    "ratings": "6.2/10",
+    "ratings": 3,
     "released": "2018",
     "genre": "Action, Comedy, Crime, Thriller",
     "plot": "Oceans Eight is a movie starring Sandra Bullock, Cate Blanchett, and Anne Hathaway. Debbie Ocean gathers an all-female crew to attempt an impossible heist at New York Citys yearly Met Gala.",
     "actors": "Sandra Bullock, Griffin Dunne, Deidre Goodwin, Daniella Rabbani."
   },
   "The Marine 6 (2018)" : {
-    "ratings": "5/10",
+    "ratings": 2,
     "released": "2018",
     "genre": "Action",
     "plot": "After deadly terrorists abduct his niece at a soccer match, an ex-soldier with lethal fighting skills wages a one-man war to save her and prevent mass destruction.",
     "actors":"Mike 'The Miz' Mizanin, Shawn Michaels, Rebecca Quin, Louisa Connolly-Burnham."
   },
   "The Equalizer 2 (2018)" : {
-    "ratings": "5.4/10",
+    "ratings": 2,
     "released": "2018",
     "genre": "Action, Drama",
     "plot": "The Marine 6: Close Quarters is a movie starring Mike The Miz Mizanin, Shawn Michaels, and Rebecca Quin. Jake Carter and another former Marine, Luke Trapper, join forces to rescue a kidnapped girl from a gang.",
     "actors":"Denzel Washington, Pedro Pascal, Ashton Sanders, Orson Bean."
   },
   "Year One (2009)" : {
-    "ratings": "4.9/10",
+    "ratings": 1,
     "released": "2018",
     "genre": "Adventure, Comedy",
     "plot": "Year One is a movie starring Jack Black, Michael Cera, and Olivia Wilde. After being banished from their tribe, two hunter-gatherers encounter Biblical characters and eventually wind up in the city of Sodom.",
     "actors":"Jack Black, Michael Cera, Oliver Platt, David Cross, Christopher Mintz-Plasse."
   },
   "Yes Man (2008)" : {
-    "ratings": "6.8/10",
+    "ratings": 3,
     "released": "2018",
     "genre": "Comedy, Romance",
     "plot": "Yes Man is a movie starring Jim Carrey, Zooey Deschanel, and Bradley Cooper. A man challenges himself to say yes to everything for an entire year.",
     "actors":"Jim Carrey, Zooey Deschanel, Bradley Cooper, John Michael Higgins."
   },
   "The Wedding Singer (1998)" : {
-    "ratings": "6.8/10",
+    "ratings": 4,
     "released": "2018",
     "genre": "Comedy, Music, Romance",
     "plot": "The Wedding Singer is a movie starring Adam Sandler, Drew Barrymore, and Christine Taylor. Robbie, a singer, and Julia, a waitress, are both engaged, but to the wrong people. Fortune intervenes to help them discover each other.",
