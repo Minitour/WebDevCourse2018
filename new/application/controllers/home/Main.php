e9c2e666c60faf1dad621624ca6c409430b31266
Tony Zaitoun <tony.z.1711@gmail.com>
2019-02-15 13:47:01 +0200

    fixed destory session

--- new/application/controllers/home/Main.php
+++ new/application/controllers/home/Main.php
@@ -43,7 +43,9 @@ class Main extends CI_Controller{
     }
 
     public function login_view() {
-        session_destroy();
+        if (session_status() == PHP_SESSION_ACTIVE) {
+            session_destroy();
+        }
         $this->load->view("pages/login");
     }
 
