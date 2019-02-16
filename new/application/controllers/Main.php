5bafd7dbcafaa2aee52e76aa08563312b87399fb
Tony Zaitoun <tony.z.1711@gmail.com>
2019-02-16 17:04:41 +0200

    fixing registration date bug

--- new/application/controllers/Main.php
+++ new/application/controllers/Main.php
@@ -62,11 +62,11 @@ class Main extends CI_Controller{
 
     public function profile_view_self() {
         $this->redirectIfNeeded();
-        
+
         $user = $this->user_model->get_user_by_username($_SESSION['username']);
         if ($user == FALSE) {
             show_404();
-            die;
+            die();
         }
         $user_id = $user->ID;
         $query = $this->review_model->get_all_reviews($user_id);
