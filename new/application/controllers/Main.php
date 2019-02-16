744d25f59abdba0644af3faa220e49cffefc5017
Unknown <goldfedertomer@gmail.com>
2019-02-16 18:26:09 +0200

    update profile

--- new/application/controllers/Main.php
+++ new/application/controllers/Main.php
@@ -81,7 +81,7 @@ class Main extends CI_Controller{
                 $query = $this->movie_model->get_movie_details($review->movie_id);
                 $movie = $query->result();
                 //var_dump($movie);
-                $temp_data['movie_name'] = $movie->name;
+                $temp_data['movie_name'] = $movie[0]['name'];
                 $temp_data['comment'] = $review->comment;
                 $temp_data['star_rating'] = $review->star_rating;
                 $temp_data['created_at'] = $review->created_at;
